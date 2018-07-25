434c4a2a2313c7e19c39f8d1480a3190913ccab6
Thibaud Jeannin <thibaud.jeannin@gmail.com>
2018-07-25 14:52:46 +0200

    Continué les Builders en S6. Fait les méthodes build, on peut maintenant construire le réseau de neuronnes avec build()

--- R/RuralNetwork.r
+++ R/RuralNetwork.r
@@ -1,29 +1,31 @@
 library(R6)
+library(magrittr)
 
 RuralNetwork <- R6Class(
   "RuralNetwork",
   public = list(
     
     initialize = function(layers = list()) {
-      private$layers <- layers
+      private$m_layers <- layers
     },
     
-    print = function(...) {
-      cat (sprintf("Neural Network"))
-      for (l in seq(self$layers)) {
-        print( self$layers[[i]] )
+    print = function() {
+      cat(sprintf('RuralNetwork with %s layers', length(private$m_layers)))
+      cat('\n')
+      for (i in seq(length(private$m_layers)) ) {
+        cat('| ')
+        print(private$m_layers[[i]])
+        cat('\n')
       }
     }
     
   ),
   private = list(
     
-    layers = NULL
+    m_layers = NULL
     
   )
 )
-
-nnet <- RuralNetwork$new(list(42,43))
 
 RuralNetworkBuilder <- R6Class(
   "RuralNetworkBuilder",
@@ -34,18 +36,102 @@ RuralNetworkBuilder <- R6Class(
     },
     
     build = function() {
-      
+      layers <- list()
+      for (i in seq(length(private$m_layers)) ) {
+        if (i == 1) {
+          layers %<>% append(private$m_layers[[i]]$build())
+        } else {
+          layers %<>% append(private$m_layers[[i]]$build( private$m_layers[[i-1]]$size() ))
+        }
+      }
+      return(RuralNetwork$new(layers = layers))
+    },
+
+    print = function() {
+      cat(sprintf('RuralNetworkBuilder with %s layers', length(private$m_layers)))
+      cat('\n')
+      for (i in seq(length(private$m_layers)) ) {
+        cat('| ')
+        print(private$m_layers[[i]])
+        cat('\n')
+      }
     },
     
     withLayer = function(layerBuilder) {
-      
+      private$m_layers %<>% append(layerBuilder)
+      return(self)
     }
     
   ),
   
   private = list(
     
-    layers = NULL
+    m_layers = NULL
     
   )
 )
+
+Layer <- R6Class(
+  "Layer",
+  public = list(
+    
+    initialize = function(size, size_prev_layer, type="simple") {
+      private$m_type <- type
+      private$m_neurons <- matrix(
+        nrow = size,
+        ncol = 1
+      )
+      private$m_neurons <- matrix(
+        runif(size * size_prev_layer, 0, 1), nrow = size, ncol = size_prev_layer)
+    },
+    
+    print = function() {
+      cat (sprintf('Layer (%s) with %s neurons', private$m_type, nrow(private$m_neurons)))
+    }
+    
+  ),
+  private = list(
+    m_type = NULL,
+    m_neurons = NULL
+    
+  )
+)
+
+LayerBuilder <- R6Class(
+  "Layer",
+  public = list(
+
+    initialize = function(size = 1, type="simple") {
+      private$m_type = type
+      private$m_size = size
+
+    },
+
+    print = function() {
+      cat (sprintf('LayerBuilder (%s) with %s neurons', private$m_type, private$m_size))
+    },
+    
+    build = function(size_prev = 0) {
+      return(Layer$new(type = "simple", size = private$m_size, size_prev = size_prev))
+    },
+    
+    size = function() { return(private$m_size) }
+
+  ),
+  private = list(
+    
+    m_type = NULL,
+    m_size = NULL
+    
+  )
+)
+
+nnetB <- RuralNetworkBuilder$new()$
+  withLayer(LayerBuilder$new(2))$
+  withLayer(LayerBuilder$new(3))$
+  withLayer(LayerBuilder$new(1))
+
+print(nnetB)
+
+nnet <- nnetB$build()
+print(nnet)
